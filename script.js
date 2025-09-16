a08296130c0968bdad2e18ad35b3db69be37b6b0
srikanthnagudu2909 <nagudusrikanth@gmail.com>
2025-09-16 23:14:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-
 function insert_Row() {
-    //Write your code here
-	  const table=document.getElementById("sampleTable")
-      const tr=document.createElement("tr")
-      const td1=document.createElement("td")
-      td1.innerText="New Cell1"
-      const td2=document.createElement("td")
-      td2.innerText="New Cell2"
-      tr.append(td1,td2)
-      table.insertBefore(tr,table.firstChild)
-  
-  
+  const table = document.getElementById("sampleTable");
+  const newRow = table.insertRow(0); // always insert at top (inside tbody)
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+  cell1.innerText = "New Cell1";
+  cell2.innerText = "New Cell2";
 }
