4a7619b880e13add86cc661462d61ec26af1c55f
srikanthnagudu2909 <nagudusrikanth@gmail.com>
2025-09-16 22:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let count=1
 function insert_Row() {
     //Write your code here
+	  const table=document.getElementById("sampleTable")
+      const tr=document.createElement("tr")
+      const td1=document.createElement("td")
+      td1.innerText=`NewCell${count++}`
+      const td2=document.createElement("td")
+      td2.innerText=`NewCell${count}`
+      tr.append(td1,td2)
+      table.insertBefore(tr,table.firstChild)
   
   
 }
